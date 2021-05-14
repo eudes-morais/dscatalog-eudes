41cfc816c9ad7890f7eaf60be28ab03b5e424a0e
Eudes Morais <eudes.morais@gmail.com>
2021-05-14 00:47:38 -0300

    DELETE Category

--- backend/src/main/java/com/eudes/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/eudes/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.eudes.dscatalog.dto.CategoryDTO;
 import com.eudes.dscatalog.entities.Category;
 import com.eudes.dscatalog.repositories.CategoryRepository;
+import com.eudes.dscatalog.services.exceptions.DatabaseException;
 import com.eudes.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service // Annotation que registra a classe como parte do sistema de injeção de dependências do sistema
@@ -61,5 +64,19 @@ public class CategoryService {
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found: " + id);
 		}
+	}
+
+	public void delete(Long id) {
+		
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
 	}
  }

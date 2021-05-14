de0d7e8cac679144d07173511b5694d86b35c1db
Eudes Morais <eudes.morais@gmail.com>
2021-05-14 00:30:54 -0300

    PUT Category

--- backend/src/main/java/com/eudes/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/eudes/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.eudes.dscatalog.dto.CategoryDTO;
 import com.eudes.dscatalog.entities.Category;
 import com.eudes.dscatalog.repositories.CategoryRepository;
-import com.eudes.dscatalog.services.exceptions.EntityNotFoundException;
+import com.eudes.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service // Annotation que registra a classe como parte do sistema de injeção de dependências do sistema
 public class CategoryService {
@@ -31,7 +33,7 @@ public class CategoryService {
 	public CategoryDTO findById(Long id) {
 		
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		
 		return new CategoryDTO(entity);
 	}
@@ -44,5 +46,20 @@ public class CategoryService {
 		entity = repository.save(entity);
 		
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		
+		try {
+			Category entity = repository.getOne(id);
+			entity.setName(dto.getName());		
+			entity = repository.save(entity);
+			
+			return new CategoryDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		}
 	}
  }

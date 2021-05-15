b78c9ae20453d03c42e11f3ac045b989d5c782ca
Eudes Morais <eudes.morais@gmail.com>
2021-05-15 15:10:50 -0300

    Category Pagination

--- backend/src/main/java/com/eudes/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/eudes/dscatalog/services/CategoryService.java
@@ -9,6 +9,8 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,11 +27,11 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)	
-	public List<CategoryDTO> findAll() {
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
 		
-		List<Category> list = repository.findAll();
+		Page<Category> list = repository.findAll(pageRequest);
 		
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		return list.map(x -> new CategoryDTO(x)); // Como o PAGE já é um STREAM do JAVA 8, não precisa do método STREAm e nem do COLLECT
 	}
 
 	@Transactional(readOnly = true)

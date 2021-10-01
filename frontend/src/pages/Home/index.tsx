31f505a671b412686dd45999909e5551dad75c80
Eudes Morais <eudes.morais@gmail.com>
2021-10-01 19:42:06 -0300

    Starting  style

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg'
 import Navbar from "components/Navbar";
+import './styles.css'
 
 const Home = () => {
     return (
@@ -8,7 +9,7 @@ const Home = () => {
         <div className="home-container">
             <div className="home-card">
                 <div className="home-content-container">
-                    <h1>Conheça o melhor catálogo</h1>
+                    <h1>Conheça o melhor catálogo de produtos</h1>
                 </div>
                 <div className="home-image-container">
                     <MainImage />

d053d52f1b5c00abcedff8fb9190aad8c8b12121
Eudes Morais <eudes.morais@gmail.com>
2021-10-06 00:45:12 -0300

    Configure routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import './assets/styles/custom.scss'
 import './App.css';
-import Catalog from 'pages/Home/Catalog';
-// import Home from 'pages/Home';
+import Routes from 'Routes';
 
 function App() {
   return (
-    // <Home />
-    <Catalog />
+    <Routes />
   );
 }
 
